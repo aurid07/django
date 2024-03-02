e746ea7f2e27020184f99e83bb756c61673ab968
ashir41 <ashirintheshar456@gmail.com>
2024-03-03 00:24:20 +0600

    templates

--- core/views.py
+++ core/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(requests):
-    return HttpResponse('<h1>welcome to social book</h1>')
+    return render(request, 'index.html')
+
