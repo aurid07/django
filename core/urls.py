ffc117f73c1fbcfe7432496d7ceb68e44fb626ec
PeteCantCode <utsa770@gmail.com>
2024-03-03 15:04:22 +0600

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('signin', views.signin, namne='signin'),
+    path('logout', views.logout, namne='logout'),
 
 ]
