33c4aafb0e0e720511ef64f18c978278de33375e
PeteCantCode <utsa770@gmail.com>
2024-03-03 15:02:32 +0600

    added static files and sign in and logout

--- core/views.py
+++ core/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+<<<<<<< Updated upstream
 def index(requests):
     return render(request, 'index.html')
 
+=======
+
+def signin(request):
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
+        
+    else:
+        return render(request, 'signin.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
+>>>>>>> Stashed changes
