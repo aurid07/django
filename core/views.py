7a151a086a595cc20aeb0ffbc78a9017eecb6d1c
PeteCantCode <utsa770@gmail.com>
2024-03-03 15:03:32 +0600

    added static files and sign in and logout

--- core/views.py
+++ core/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from django.http import HttpResponse
-# Create your views here.
-<<<<<<< Updated upstream
-def index(requests):
-    return render(request, 'index.html')
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 
-=======
+# Create your views here.
+@login_required(login_url='signin')
+def index(request):
+    return render(request, 'index.html')
 
 def signin(request):
     
@@ -25,7 +28,7 @@ def signin(request):
     else:
         return render(request, 'signin.html')
 
+@login_required(login_url='signin')
 def logout(request):
     auth.logout(request)
     return redirect('signin')
->>>>>>> Stashed changes
