dea86f6f4ae03888c66ec57cf39e7581fd4a34ee
aurid07 <araurid@gmail.com>
2024-03-03 22:09:45 +0600

    fixing errors

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, namne='signin'),
+    path('logout', views.logout, namne='logout'),
 
 ]
