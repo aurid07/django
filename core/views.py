11eba1b17091b80eeee59d9f27832fc1868ae09a
Shahidul-Islam-Fuad <149116296+Shahidul-Islam-Fuad@users.noreply.github.com>
2024-03-04 09:22:56 +0600

    Account Settings done

--- core/views.py
+++ core/views.py
@@ -9,6 +9,35 @@ from .models import Profile
 @login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        return redirect('settings')
+    return render(request, 'setting.html', {'user_profile': user_profile})
+
 def signup(request):
 
     if request.method == 'POST':
@@ -16,6 +45,11 @@ def signup(request):
         email = request.POST['email']
         password = request.POST['password']
         password2 = request.POST['password2']
+
+        # Add a check for empty fields
+        if not username or not email or not password:
+            messages.error(request, 'Please fill in all the required fields')
+            return redirect('signup')
 
         if password == password2:
             if User.objects.filter(email=email).exists():
@@ -29,16 +63,18 @@ def signup(request):
                 user.save()
 
                 #log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
 
                 #create a Profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user= user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('settings')
                 
 
         else:
-            messages.info(request, 'Passwoed Not Matching' )
+            messages.info(request, 'Password Not Matching' )
             return redirect('signup')
     else:
         return render(request, 'signup.html')
